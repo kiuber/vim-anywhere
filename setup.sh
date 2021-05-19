3abd3e1997777e61a9927efe98ddf25db5404f01
kiuber <kiuber.zhang@gmail.com>
2021-05-20 00:56:07 +0800

    update: clone Vundle.vim.git --depth=1

--- setup.sh
+++ setup.sh
@@ -12,6 +12,6 @@ exe_cmd "cp -rf $root_dir/files/vimfiles $share_dir/"
 exe_cmd "ln -sf $share_dir/_vimrc $HOME/.vimrc"
 exe_cmd "ln -sf $share_dir/vimfiles $HOME/.vim"
 
-exe_cmd "git clone https://github.com/VundleVim/Vundle.vim.git $share_dir/bundle/Vundle.vim"
+exe_cmd "git clone https://github.com/VundleVim/Vundle.vim.git --depth=1 $share_dir/bundle/Vundle.vim"
 exe_cmd 'echo | vim +PluginInstall +qall'
 
